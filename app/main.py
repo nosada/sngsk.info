63c413e2918dc0e19d5eb3c1eaafa68022dcd44c
NOGISAKA Sadata <ngsksdt@gmail.com>
2017-06-28 22:23:37 +0900

    change directory hierarchy for app

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ FAVICON = CONF.get(SECTION, "favicon")
 APP = flask.Flask(__name__)
 
 
-class LoremIpsumBody(object):
+class GenerateBody(object):
     """Generate Lorem Ipsum content and put it into page body"""
 
     def __init__(self):
@@ -34,19 +34,19 @@ class LoremIpsumBody(object):
 
         section = "lorem_ipsum"
         word_list = CONF.get(section, "word_source")
-        self.rate_emphasis = int(CONF.get(section, "rate_emphasis"))
         word_source = os.path.join(
             SCRIPT_LOCATION, "files", word_list)
 
         self.lorem_ipsum = LoremIpsum(word_source)
         self.text = self.lorem_ipsum.generate_text()
         self.emphasis = [" <b>{}</b> ", " <i>{}</i> "]
+        self.rate_emphasis = int(CONF.get(section, "rate_emphasis"))
 
     def generate_html_body(self):
         """Create page body; generate Lorem Ipsum and put it into page body"""
 
         paragraphs = self.text.split("\n\n")
-        html_paragraphs = set()
+        html_paragraphs = []
         for paragraph in paragraphs:
             html_paragraph = ''
             for word in paragraph.split(' '):
@@ -54,7 +54,7 @@ class LoremIpsumBody(object):
                     html_paragraph += random.choice(self.emphasis).format(word)
                 else:
                     html_paragraph += " {} ".format(word)
-            html_paragraphs.add("<p>" + html_paragraph + "</p>")
+            html_paragraphs.append("<p>" + html_paragraph + "</p>")
         html_body = '\n'.join(html_paragraphs)
         return html_body
 
@@ -62,7 +62,7 @@ class LoremIpsumBody(object):
 @APP.route('/')
 def return_lorem_ipsum_html():
     """Generate Lorem Ipsum page and return it"""
-    lib = LoremIpsumBody()
+    lib = GenerateBody()
     html_body = lib.generate_html_body()
     return flask.render_template("index.html.tmpl",
                                  body=html_body,
